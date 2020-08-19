fa2271550d7acb2e85236744dda5897d873cf438
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-19 22:31:13 +0100

    Update c-w2-final-pt1.py

--- c-w2-final-pt1.py
+++ c-w2-final-pt1.py
@@ -1,4 +1,4 @@
-# Remove punctuation
+# 1) Remove punctuation
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 
 def strip_punctuation(s):
@@ -6,7 +6,8 @@ def strip_punctuation(s):
         if i in punctuation_chars:
             s = s.replace(i, "")
     return s
-#
+# 2) Next, copy in your strip_punctuation function and define a function called get_pos which takes one parameter, a string which represents one or more sentences, and calculates how many words in the string are considered positive words. Use the list, positive_words to determine what words will count as positive. The function should return a positive integer - how many occurrences there are of positive words in the text. Note that all of the words in positive_words are lower cased, so you’ll need to convert all the words in the input string to lower case as well.
+
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 # list of positive words to use
 positive_words = []
