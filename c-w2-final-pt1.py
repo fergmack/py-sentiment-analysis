bb3d3ea2d755fe8f6a40fba8e9e8efb6d4e63b64
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-19 22:30:33 +0100

    Update c-w2-final-pt1.py

--- c-w2-final-pt1.py
+++ c-w2-final-pt1.py
@@ -6,4 +6,16 @@ def strip_punctuation(s):
         if i in punctuation_chars:
             s = s.replace(i, "")
     return s
-            
+#
+punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
+# list of positive words to use
+positive_words = []
+with open("positive_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            positive_words.append(lin.strip())
+
+def strip_punctuation(s):
+    for i in s:
+        if i in punctuation_chars:
+            s = s.r
