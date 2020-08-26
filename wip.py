585fa6c29288c8560f9bc0c5272925a3b51d92c7
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-26 20:56:05 +0100

    Update wip.py

--- wip.py
+++ wip.py
@@ -1,19 +1,23 @@
-tweets = open('tweets.txt', 'r')
+tweets = open('project_twitter_data.csv', 'r')
 resulting_data = open('resulting_data.csv', 'w')
 
-
-
-# get rid of these characters
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 
 positive_words = ['yes']
-### Strip out useless characters
+### Strip out useless characters
 def strip_punctuation(s):
     for i in s:
         if i in punctuation_chars:
             s = s.replace(i, "")
-    return s
-
+            
+            
+# lists of words to use
+positive_words = []
+with open("positive_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            positive_words.append(lin.strip())
+            
 # count positives 
 def get_pos(sentence):
     count_pos = 0
@@ -24,7 +28,12 @@ def get_pos(sentence):
             count_pos +=1
     return count_pos  
 
-# count negatives 
+negative_words = []
+with open("negative_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            negative_words.append(lin.strip())
+
 negative_words = ['no']
 def get_neg(sentence):
     count_neg = 0
@@ -33,8 +42,9 @@ def get_neg(sentence):
     for w in words:
         if w in negative_words:
             count_neg += 1
-    return count_neg     
+    return count_neg    
 
+## this is function 
 def thisisx(resulting_data):
     tweets_header = tweets.readlines()
 # what is happening here. Why is header popped in the for loop?
@@ -44,13 +54,9 @@ def thisisx(resulting_data):
 
     # no retweets, replies, score pos, socre neg, net
     for line in tweets_header:
-      line_list = line.strip().split(',')
+        line_list = line.strip().split(',')
       # print(line_list)r
-      resulting_data.write("{}, {}, {}, {}".format(line_list[1], line_list[2],
+        resulting_data.write("{}, {}, {}, {}".format(line_list[1], line_list[2],
       get_pos(line_list[0]), get_neg(line_list[0]),   (get_pos(line_list[0])-get_neg(line_list[0]))  )
       )
-      resulting_data.write('\n')
-
-# x = resulting_data.readlines()
-# for l in x:
-#   print (l)
+        resulting_data.write('\n')
