815fcefe57daeeca1ac97a394deefdd7652f8dfa
fergmack <33000542+fergmack@users.noreply.github.com>
2020-08-29 21:39:48 +0100

    Update c-w2-final-pt1.py

--- c-w2-final-pt1.py
+++ c-w2-final-pt1.py
@@ -52,3 +52,67 @@ def get_neg(sentence):
         if w in negative_words:
             count_neg += 1
     return count_neg
+
+## final code 
+# Open list of positive/negative words
+positive_words = []
+with open("positive_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            positive_words.append(lin.strip())
+            
+negative_words = []
+with open("negative_words.txt") as neg_f:
+    for lin in neg_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            negative_words.append(lin.strip())
+            
+# remove unwanted characters
+punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']  
+def strip_punctuation(word):
+    for punctChar in punctuation_chars:
+        word = word.replace(punctChar, '')
+    return word
+# Count pos/neg
+def get_pos(sentences):
+    sentences = strip_punctuation(sentences)
+    sentenceList = sentences.split()
+    count = 0 
+    for word in sentenceList:
+        for posWord in positive_words:
+            if word == posWord:
+                count +=1
+    return count
+
+def get_neg(sentences):
+    sentences = strip_punctuation(sentences)
+    sentenceList = sentences.split()
+    count = 0
+    for word in sentenceList:
+        for negWord in negative_words:
+            if word == negWord:
+                count += 1
+    return count
+
+
+twitterDataFile = open("project_twitter_data.csv","r")
+resultingDataFile = open("resulting_data.csv","w")
+
+
+# perform analysis 
+def twitterResults(resultingDataFile):
+    resultingDataFile.write("Number of Retweets, Number of Replies, Positive Score, Negative Score, Net Score")
+    resultingDataFile.write("\n")
+
+    linesPTDF =  twitterDataFile.readlines()
+    headerDontUsed= linesPTDF.pop(0)
+    for linesTD in linesPTDF:
+        listTD = linesTD.strip().split(',')
+        resultingDataFile.write("{}, {}, {}, {}, {}".format(listTD[1], listTD[2], get_pos(listTD[0]), get_neg(listTD[0]), (get_pos(listTD[0])-get_neg(listTD[0]))))    
+        resultingDataFile.write("\n")
+
+        
+
+twitterResults(resultingDataFile)
+twitterDataFile.close()
+resultingDataFile.close()
